70517468a5caa36cebc09bf45b40e8db9821a1bf
Ramiro Andino <ramiroandino987@gmail.com>
2024-12-08 13:26:54 -0500

    log in page visuals

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/*.{html,js}"
-],
+  content: ["./dist/**/*.{html,js}"],
+  darkMode: 'class',
+
+
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        amarilloneek: "#CB9F47",
+        azuloscuro: "#0D101A"
+    },
+    
   },
   plugins: [require('flowbite/plugin')],
-}
+  }
+}  
